a7e0e35ffd0321bcc71b90ade3cd9935faf21f6a
uniquemj <maharjanyunik77@gmail.com>
2026-03-12 22:44:58 +0545

    fix(): conditional logic.

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,12 @@ const App = () => {
   return (
     <div className="w-full h-dvh overflow-x-hidden px-space-43 py-space-24 bg-gradient-to-b from-black to-primary-800 no-scrollbar">
       {
-        InConstruction ? (
+        InConstruction ? <Construction/> : (
           <>
             <Navbar/>
             <Header/>
           </>
-        ):
-        <Construction/>
+        )
       }
     </div>
   )
