48ea500796dfd055f63e6f4a9bf38b0a8d03f35a
uniquemj <maharjanyunik77@gmail.com>
2026-03-12 22:42:59 +0545

    fix(): env conditional logic.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Construction from "@/components/Construction"
 
 
 const App = () => {
-  const InConstruction = import.meta.env.VITE_INCONSTRUCTION
+  const InConstruction = import.meta.env.VITE_INCONSTRUCTION === "true"
   console.log(InConstruction)
   return (
     <div className="w-full h-dvh overflow-x-hidden px-space-43 py-space-24 bg-gradient-to-b from-black to-primary-800 no-scrollbar">
