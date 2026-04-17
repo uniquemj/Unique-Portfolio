c5a3aadde179b373de5631e48d53443f2f9068c8
Unique Maharjan <maharjanyunik77@gmail.com>
2026-04-17 23:46:44 +0545

    feat: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
-import Navbar from "@/container/Navbar";
-import Header from "@/container/Header";
-import ComingSoon from "@/container/ComingSoon";
-import Skills from "@/container/Skills";
-
 const App = () => {
-  const InConstruction = import.meta.env.VITE_INCONSTRUCTION === "true";
-  console.log(InConstruction);
   return (
-    <main>
-      <Navbar />
-      <Header />
-      <Skills/>
-      <ComingSoon />
-    </main>
-  );
-};
+    <div>App</div>
+  )
+}
 
-export default App;
+export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    },
-  },
+    }
+  }
 })
